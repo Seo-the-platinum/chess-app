91d26f8e8f48936e4a0a69725d55e20c4b7c930f
Eliseo Flores <eliseo_flores530@yahoo.com>
2021-09-30 15:02:32 -0700

    removed commented section

--- frontend/chessboard/src/referee.js
+++ frontend/chessboard/src/referee.js
@@ -26,24 +26,6 @@ export default class Referee {
           return true
         }
       }
-      /*
-      if (py === specialRow) {
-        if (px === x && y - py === 1*pawnDirection) {
-          if (!this.tileIsOccupied(x,y,boardState)) {
-            return true;
-          }
-        } else if (px === x && y - py === 2*pawnDirection) {
-          if (!this.tileIsOccupied(x,y,boardState) && !this.tileIsOccupied(x,y-pawnDirection, boardState)) {
-            return true
-          }
-        }
-      } else {
-        if (px === x && y - py === pawnDirection) {
-          if (!this.tileIsOccupied(x,y,boardState)) {
-            return true
-          }
-        }
-      }*/
     }
     return false
   }
